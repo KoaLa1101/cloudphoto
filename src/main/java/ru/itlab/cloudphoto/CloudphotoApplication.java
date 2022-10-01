bbdc22ec05612e2c32be1323d1793a1411c3bd23
koala1101 <rproba@list.ru>
2022-10-01 21:30:17 +0300

    init

---
+++ src/main/java/ru/itlab/cloudphoto/CloudphotoApplication.java
@@ -0,0 +1,12 @@
+package ru.itlab.cloudphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CloudphotoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudphotoApplication.class, args);
+    }
+}
